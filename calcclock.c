8ca87cd5ad40411e53d09c18a90409c06272118c
gyeonghyeon-kim <leonardkkh@gmail.com>
2020-11-13 16:31:47 +0900

    timespec timer

---
+++ calcclock.c
@@ -0,0 +1,22 @@
+#define BILLION 1000000000
+
+unsigned long long calclock3(struct timespec64 *spclock, unsigned long long *total_time, unsigned long long *total_count){
+    long temp, temp_n;
+    unsigned long long timedelay = 0;
+    if(spclock[1].tv_nsec >= spclock[0].tv_nsec)
+    {
+        temp = spclock[1].tv_sec - spclock[0].tv_sec;
+	temp_n = spclock[1].tv_nsec - spclock[0].tv_nsec;
+	timedelay = BILLION * temp + temp_n;
+    }
+    else
+    {
+        temp = spclock[1].tv_sec - spclock[0].tv_sec - 1;
+	temp_n = BILLION + spclock[1].tv_nsec - spclock[0].tv_nsec;
+	timedelay = BILLION * temp + temp_n;
+    }
+    
+    __sync_fetch_and_add(total_time, timedelay);
+    __sync_fetch_and_add(total_count, 1);
+    return timedelay;
+}
